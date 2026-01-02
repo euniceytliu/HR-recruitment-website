db27a45a76c8a72c66deab7a16ae8df1e19c517e
Eunice <eunice@tencent.com>
2026-01-03 00:22:45 +0800

    debug: show env var details

--- api/test.js
+++ api/test.js
@@ -1,10 +1,16 @@
 module.exports = (req, res) => {
+  const supabaseUrl = process.env.SUPABASE_URL;
+  const supabaseKey = process.env.SUPABASE_ANON_KEY;
+  
   res.status(200).json({ 
     success: true, 
     message: 'API is working!',
     env: {
-      hasSupabaseUrl: !!process.env.SUPABASE_URL,
-      hasSupabaseKey: !!process.env.SUPABASE_ANON_KEY
+      hasSupabaseUrl: !!supabaseUrl,
+      hasSupabaseKey: !!supabaseKey,
+      supabaseUrlLength: supabaseUrl ? supabaseUrl.length : 0,
+      supabaseKeyLength: supabaseKey ? supabaseKey.length : 0,
+      supabaseUrlFirstChars: supabaseUrl ? supabaseUrl.substring(0, 20) : 'none'
     }
   });
 };
